f827377caf4ff8dc39097b39221b33fd83f5bdc3
vpalazhica <57239284+vpalazhica@users.noreply.github.com>
2019-11-11 09:39:55 -0500

    Update step2-verify.sh

--- portworx-volumes/step2-verify.sh
+++ portworx-volumes/step2-verify.sh
@@ -1,1 +1,1 @@
-(($(ssh -o strictHostKeyChecking=no node01 pxctl service pool show  | grep  "Pool ID:") == "Pool ID: 0" )) && (( $(ssh -o strictHostKeyChecking=no node01 pxctl service pool show  | grep  "Pool ID:" | wc -l) == "1" )) && echo "done"
+(($(ssh -o strictHostKeyChecking=no node01 pxctl service pool show  | grep  "Pool ID: 1") == "Pool ID: 1" )) && (( $(ssh -o strictHostKeyChecking=no node01 pxctl service pool show  | grep  "Pool ID:" | wc -l) == "2" )) && echo "done"
