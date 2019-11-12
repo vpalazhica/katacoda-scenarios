e3e752c2b281f7375f971e682f5fc5eee55ff646
vpalazhica <57239284+vpalazhica@users.noreply.github.com>
2019-11-12 10:03:22 -0500

    Update poolcheck.sh

--- portworx-volumes/assets/poolcheck.sh
+++ portworx-volumes/assets/poolcheck.sh
@@ -1,1 +1,1 @@
-poolcount=`ssh -o strictHostKeyChecking=no root@node01 bash -c \"pxctl service pool show\" | grep ID | wc -l`; [ $poolcount -eq '2' ]
+poolcount=`ssh -t -o strictHostKeyChecking=no root@node01 bash -c \"pxctl service pool show\" | grep ID | wc -l`; [ $poolcount -eq '2' ]
