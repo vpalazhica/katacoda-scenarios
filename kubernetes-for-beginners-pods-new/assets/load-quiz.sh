7d270ad5122ca1d57bdd687b4478f5631a955ac1
vpalazhi <vijin.palazhi@gmail.com>
2020-05-08 17:37:49 -0400

    test script move

--- kubernetes-for-beginners-pods-new/assets/load-quiz.sh
+++ kubernetes-for-beginners-pods-new/assets/load-quiz.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 cd /tmp/
 
-until [ `kubectl get nodes | grep "NotReady" | wc -l` -eq 0 ] ; do
-  printf '.'
-  sleep 1
-done
 
-cd /tmp/
-launch.sh
 echo "testing port: Success" | nc -l 8080 &
 
--- kubernetes-for-beginners-pods-new/init-env.sh
+++ kubernetes-for-beginners-pods-new/init-env.sh
@@ -1,4 +1,5 @@
 sleep 2
-ssh root@host01 "chmod 755 /tmp/load-quiz.sh; /tmp/load-quiz.sh > /tmp/load-quiz.out"
+ssh root@host01 "touch /tmp/load-quiz.out"
+ssh root@host01 "chmod 755 /tmp/load-quiz.sh; /tmp/load-quiz.sh > /tmp/load-quiz0new.out"
 # ifconfig
 # chmod 755 /tmp/load-quiz.sh; /tmp/load-quiz.sh > /tmp/load-quiz.out
